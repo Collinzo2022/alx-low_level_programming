b35a8b05457361fc0c32fa97729cd49193e6a66f
Collinzo <aefuetanu@gmail.com>
2023-02-26 08:21:55 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ * main - main function
+ *
+ * Return: 0
+ **/
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+
+	return (1);
+}
