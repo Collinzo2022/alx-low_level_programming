e44bda3f735565de1c5598e0d7531cbbefebe974
Collinzo <aefuetanu@gmail.com>
2023-02-27 10:34:21 -0800

    infinite

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function prints the length of a string
+ * @s: the string
+ *
+ * Return:0.
+ **/
+int _strlen(char *s)
+{
+	int length = 0;
+	while (*s != '\0')
+	
+		length++;
+		
+	return (length);
+	
+
+}
