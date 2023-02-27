6934c23f51f3fd648b2a77b75a02784fc3db027f
Collinzo <aefuetanu@gmail.com>
2023-02-27 09:40:38 -0800

    Pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - prints a number to a pointer and returns the value.
+ * @n: the number to be updated
+ * Return: nothing on success.
+ **/
+void reset_to_98(int *n)
+{
+	 *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is about types of variables,pointers, strings and arrays
