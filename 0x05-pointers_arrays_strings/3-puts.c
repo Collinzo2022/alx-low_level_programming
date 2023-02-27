df465c2f87f7083128c25c6fab6a1bd87ecd5bbb
Collinzo <aefuetanu@gmail.com>
2023-02-27 11:11:10 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string
+ * Return: 0.
+ **/
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+
+
+}
