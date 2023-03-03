42911dac352ba6ec17e1e798991dd9bc5a42e540
Collinzo <aefuetanu@gmail.com>
2023-03-02 17:14:33 -0800

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
-	{ 
+	{
 		s1++;
 		s2++;
 
@@ -19,7 +19,7 @@ int _strcmp(char *s1, char *s2)
 
 	{
 		return (0);
-	
+
 	}
 
 	else
@@ -27,7 +27,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		return (*s1 - *s2);
 
-	
-	} 
+
+	}
 
 }
