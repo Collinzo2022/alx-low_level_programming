5d10df61b3c1033408dd291bfed841255ac56711
Collinzo2022 <aefuetanu@gmail.com>
2023-06-19 16:45:11 -0700

    Structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct function.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner.
+ **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
