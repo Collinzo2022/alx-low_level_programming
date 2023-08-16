a1c0dc6bb3c1abbc6fdf1e7cde6ef6c426e9b596
Collinzo <aefuetanu@gmail.com>
2023-08-15 21:32:05 -0700

    struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * free_dog - frees memory for dog.
+ * @d: dog_t variable.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
