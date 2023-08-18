1d68700eb830db310553baa67d5b032bdd12b974
Collinzo <aefuetanu@gmail.com>
2023-08-17 20:06:45 -0700

    str

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,65 @@
 #include "variadic_functions.h"
 /**
+ * print_char - prints a char.
+ * @arg: list of arguments pointing to
+ * character.
+ */
+void print_char(va_list arg)
+{
+	char c;
+
+	c = va_arg(arg, int);
+
+	printf("%c", c);
+}
+
+/**
+ * print_int - prints an int.
+ * @arg: list of arguments pointing
+ * to integer.
+ */
+void print_int(va_list arg)
+{
+	int num;
+
+	num = va_arg(arg, int);
+
+	printf("%d", num);
+}
+/**
+ * print_float - prints a float.
+ * @arg: list of arguments pointing
+ * to float.
+ */
+void print_float(va_list arg)
+{
+	float num;
+
+	num = va_arg(arg, double);
+
+	printf("%f", num);
+}
+/**
+ * print_string - prints a string.
+ * @arg: list of arguments pointing
+ * to string.
+ */
+void print_string(va_list arg)
+{
+	char *str;
+
+	str = va_arg(arg, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+
+	printf("%s", str);
+}
+
+/**
  * print_all - function that prints anything.
  *
  * @format: a list of types of arguments
@@ -9,44 +69,41 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
+	va_list arg;
+
 	int i = 0;
-	char *str;
+	int j = 0;
+	char *separator = " ";
 
-	va_start(args, format);
+	printer functions[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+	};
 
-	while (format && format[i])
+	va_start(arg, format);
+
+	while (format && (*(format + i)))
 	{
-		if (format[i] == 'c')
-		{
-			printf("%c", va_arg(args, int));
-		}
+		j = 0;
 
-		else if (format[i] == 'i')
+		while (j < 4 && (*(format + i) != *(functions[j].symbol)))
+			j++;
+		if (j < 4)
 		{
-			printf("%d", va_arg(args, int));
+			printf("%s", separator);
+				functions[j].print(arg);
+			separator = ", ";
 		}
-
-		else if (format[i] == 'f')
-		{
-			printf("%f", va_arg(args, double));
-		}
-		else if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s", str);
-		}
-
-		if ((format[i + 1] != '\0') && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
-			printf(", ");
 
 		i++;
 	}
 
 	printf("\n");
 
+	va_end(arg);
 
-	va_end(args);
+
+
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,8 +3,23 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+/**
+ * typedef struct print_func - a struct of
+ * symbol and function.
+ * @symbol: a member of symbol.
+ * @print: a member that is function pointer.
+ */
+typedef struct print_func
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
 #endif
